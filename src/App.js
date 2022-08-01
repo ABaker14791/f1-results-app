21c8004eb35ea3881a46edf1c136b965b9022b07
Adam Baker <adam_baker@live.co.uk>
2022-08-02 00:00:32 +0100

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import "./index.css";
+
 function App() {
-  fetch("http://ergast.com/api/f1/current/last/results.json", {
-    method: "GET",
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data.MRData.RaceTable.Races[0].Results[0].Driver.driverId);
-    });
+  const [driver, setDriver] = useState("");
+  // const [driver, setDriver] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://ergast.com/api/f1/current/last/results.json")
+      .then((res) => {
+        setDriver(
+          res.data.MRData.RaceTable.Races[0].Results[0].Driver.givenName +
+            " " +
+            res.data.MRData.RaceTable.Races[0].Results[0].Driver.familyName
+        );
+        console.log(
+          res.data.MRData.RaceTable.Races[0].Results[0].Driver.givenName +
+            " " +
+            res.data.MRData.RaceTable.Races[0].Results[0].Driver.familyName
+        );
+      });
+  }, []);
+
+  // fetch("http://ergast.com/api/f1/current/last/results.json", {
+  //   method: "GET",
+  // })
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((data) => {
+  //     // console.log(data.MRData.RaceTable.Races[0].Results[0].Driver.driverId);
+  //     console.log(data.MRData.RaceTable.Races[0]);
+  //   });
 
   // const driver =
 
   return (
-    <div>
-      <h1>F1 Results App</h1>
-      {/* <p>1st place goes to: {driver}</p> */}
+    <div className="container mx-auto">
+      <Header />
+      <p>1st place goes to: {driver}</p>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+const Header = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-center">F1 Results</h1>
+    </div>
+  );
+};
+
+export default Header;
