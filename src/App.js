f701d3824d8a59b73d3a9fbeff196c2ea534107a
Adam Baker <adam_baker@live.co.uk>
2022-08-08 21:10:00 +0100

    Driver state now object

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import Header from "./components/Header";
 import RecentSection from "./components/RecentSection";
 
 function App() {
-  const [driver, setDriver] = useState();
+  const [driver, setDriver] = useState({});
 
   useEffect(() => {
     axios
       .get("http://ergast.com/api/f1/current/last/results.json")
       .then((res) => {
-        setDriver(
-          res.data.MRData.RaceTable.Races[0].Results[1].Driver.givenName +
+        setDriver({
+          name:
+            res.data.MRData.RaceTable.Races[0].Results[1].Driver.givenName +
             " " +
-            res.data.MRData.RaceTable.Races[0].Results[1].Driver.familyName
-        );
+            res.data.MRData.RaceTable.Races[0].Results[1].Driver.familyName,
+          position: res.data.MRData.RaceTable.Races[0].Results[1].position,
+          constructor:
+            res.data.MRData.RaceTable.Races[0].Results[1].Constructor.name,
+          circuit: res.data.MRData.RaceTable.Races[0].raceName,
+          date: res.data.MRData.RaceTable.Races[0].date,
+        });
+        console.log(res.data.MRData.RaceTable.Races[0]);
       });
   }, []);
 
@@ -24,7 +31,11 @@ function App() {
     <div className="container mx-auto">
       <Header />
       <RecentSection />
-      <p>{driver}</p>
+      <p>Date: {driver.date}</p>
+      <p>Circuit: {driver.circuit}</p>
+      <p>Name: {driver.name}</p>
+      <p>Position: {driver.position}</p>
+      <p>Constructor: {driver.constructor}</p>
     </div>
   );
 }
--- src/components/RecentSection.jsx
+++ src/components/RecentSection.jsx
@@ -4,8 +4,6 @@ const RecentSection = () => {
   return (
     <div className="container mx-auto mt-8">
       <h2 className="text-2xl">Recent Results</h2>
-      <p>Circuit</p>
-      <p>Date</p>
     </div>
   );
 };
